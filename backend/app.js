2601de0587a146fe1e3219ac026be52dfe6f04bc
walegigs <foundmissing010@gmail.com>
2026-07-26 16:49:57 -0700

    new changes again

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:3000",
   "http://127.0.0.1:3000",
-  process.env.FRONTEND_URL,
+  process.env.CLIENT_URL,
 ].filter(Boolean);
 
 const corsOptions = {
